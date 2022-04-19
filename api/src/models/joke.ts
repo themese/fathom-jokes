b570d73bfbb92df0418c614c34b29ed7c73f5907
Josema Fernandez <josema.fernandez@pulemarket.com>
2022-04-19 17:50:10 +0200

    Removed commet

--- api/src/models/joke.ts
+++ api/src/models/joke.ts
@@ -1,8 +1,4 @@
 export interface Joke {
-  // "id": 187,
-  // "type": "general",
-  // "setup": "What did the late tomato say to the early tomato?",
-  // "punchline": "I’ll ketch up"
   id: number,
   type: string,
   setup: string,
